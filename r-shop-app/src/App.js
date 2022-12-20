1f63889eba03dbf890a65db0b164930c5928aa54
Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
2022-12-20 11:26:29 -0500

    basic components and router

--- r-shop-app/src/App.js
+++ r-shop-app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import {NavLink, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import Shop from './components/Shop';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Menu>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
+      </Menu>
+      <main>
+        <Routes>
+          <Route path='*' element={ <NotFound/> }></Route>
+          <Route path='/' element={ <Home/> }></Route>
+          <Route path='/blog' element={ <Blog/> }></Route>
+          <Route path='/shop' element={ <Shop/> }></Route>
+        </Routes>
+      </main>
+      <aside>
+        <h3>Sidebar</h3>
+      </aside>
+    </Container>
   );
 }
+
+const Container = styled.div`
+    max-width: 1000px;
+    padding: 40px;
+    width: 90%;
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 2fr 1fr;
+    background: #fff;
+    margin: 40px 0;
+    border-radius: 10px;
+    box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+ 
+const Menu = styled.nav`
+    width: 100%;
+    text-align: center;
+    background: #092c4c;
+    grid-column: span 2;
+    border-radius: 3px;
+ 
+    a {
+        color: #fff;
+        display: inline-block;
+        padding: 15px 20px;
+    }
+ 
+    a:hover {
+        background: #1d85e8;
+        text-decoration: none;
+    }
+`;
 
 export default App;
---
+++ r-shop-app/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return ( 
+        <div>
+            <h1>The current page don't exist!</h1>
+            <p>Sorry! But check again your path.</p>
+        </div>
+     );
+}
+ 
+export default NotFound;
