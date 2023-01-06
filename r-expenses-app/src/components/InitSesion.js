a0d5ede806c808059ede69c57ddf59c94401293d
Esneider Velandia Gonzalez <esneider.gonzalez@mercadolibre.com.co>
2023-01-06 17:35:18 -0500

    add login form

--- r-expenses-app/src/components/InitSesion.js
+++ r-expenses-app/src/components/InitSesion.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import { Header, Title, HeaderContainer} from './../elements/Header';
+import Button from './../elements/Button';
+import { ButtonContainer, Form, Input, } from './../elements/ElementsForms'
+import { ReactComponent as LoginSvg } from './../assets/images/login.svg';
+import styled from 'styled-components';
 
+const Svg = styled(LoginSvg)`
+    width: 100%;
+    max-height: 12.25rem; /* 100px */
+    margin-bottom: 1.25rem;
+`;
 const InitSesion = () => {
-    return ( <h1>Init Sesion</h1> );
+    return ( 
+        <>
+        <Helmet>
+            <title>Login</title>
+        </Helmet>
+
+        <Header>
+            <HeaderContainer>
+                <Title>Login</Title>
+                <div>
+                    <Button to="/register">Register</Button>
+                </div>
+            </HeaderContainer>
+        </Header>
+        <Form>
+            <Svg/>
+            <Input type='email' name='email' placeholder='Email'/>
+            <Input type='password' name='password' placeholder='Password'/>
+            <ButtonContainer>
+                <Button as='button' type='submit' primario>Login</Button>
+            </ButtonContainer>
+        </Form>
+    </>
+     );
 }
  
 export default InitSesion;
